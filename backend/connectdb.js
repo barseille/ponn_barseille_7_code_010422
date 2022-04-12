55347521ca911c54bb7e0f62c26fbd233c07f7ad
BarseillePonn <ponn.barseille@gmail.com>
2022-04-12 06:18:46 +0100

    connection db

---
+++ backend/connectdb.js
@@ -0,0 +1,14 @@
+const mysql = require("mysql");
+console.log("Connexion à la base de données...");
+let connectdb = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "groupomania_database",
+});
+connectdb.connect(function (err) {
+  if (err) throw err;
+  console.log("Connecté!");
+});
+
+module.exports = connectdb;
